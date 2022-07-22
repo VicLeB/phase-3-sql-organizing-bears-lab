71be9de653fa3cc939eecd21981a69ee2ba3da35
Victoria LeBel <vickileb@hotmail.com>
2022-07-21 18:24:54 -0700

    commit tree 22313842028540ad1c71c937e7b7c4650560a03f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('MR. Chocolate', 4, 'M','Brown','nice',0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M','Black','nice',1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F','Brown','nice',0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M','White','nice',0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F','Brown','nice',0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M','Brown','sad',0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F','Brown','nice',0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M','Brown','nice',0);
